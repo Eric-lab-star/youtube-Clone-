8eaf0271f9cf718ecc08c04c0d881d2cf95b9c5a
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-23 06:49:56 +0900

    fake data base

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,37 +3,42 @@ const fakeUser = {
   loggedIn: true,
 };
 
+let videos = [
+  {
+    title: "First Video",
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 58,
+    id: 1,
+    rating: 3,
+  },
+  {
+    title: "Second Video",
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 58,
+    id: 2,
+    rating: 3,
+  },
+  {
+    title: "Third Video",
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 58,
+    id: 3,
+    rating: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 58,
-      id: 1,
-      rating: 3,
-    },
-    {
-      title: "Second Video",
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 58,
-      id: 1,
-      rating: 3,
-    },
-    {
-      title: "Third Video",
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 58,
-      id: 1,
-      rating: 3,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
 
-export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("search videos");
 export const upload = (req, res) => res.send("upload vidoes");
