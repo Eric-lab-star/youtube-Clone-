8bcaa98b8940e6a81daff5b4be7498dc2f6c3b66
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-22 09:42:24 +0900

    requests and respond

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import express from "express";
-
-const app = express();
 const PORT = 4000;
+const app = express();
+
+const hanleHome = (req, res) => {
+  return res.send("<h1>I still love you</h1>");
+};
+const hanleLogin = (req, res) => {
+  return res.send("i am login");
+};
+
+app.get("/", hanleHome);
+app.get("/login", hanleLogin);
+
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}🚀`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
