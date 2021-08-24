a8d49b2a32d0c555df6afd27bd53c15a6189427e
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-25 07:12:26 +0900

    start

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,10 +6,10 @@ export const home = async (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 //edit contorollers
 export const getEdit = (req, res) => {
@@ -38,11 +38,6 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-
-      meta: {
-        views: 0,
-        rating: 0,
-      },
       hashtags: hashtags.split(",").map((word) => `#${word}`),
     });
     return res.redirect("/");
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,15 +1,15 @@
 //db에 동영상 정보를 설명해주는 역활을 한다. 구체적인 값을 주는 것이 아니라 형태를 설명하는 역활을 한다 .
-
+// 더 많은 정보는 mongoose 에서 확인
 import mongoose from "mongoose";
 //schema 에서 동영상의 모습을 설명한다
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: { type: Date, required: true },
-  hashtags: [{ type: String }],
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, maxLength: 200 },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, tirm: true }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0 },
+    rating: { type: Number, default: 0 },
   },
 });
 //모델 이름과 schema를 묶어 준다.
