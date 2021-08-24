11d82aa2c2e6a38930c63a72ade1a3c64c8ce11a
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-25 07:36:36 +0900

    edit video page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,17 +9,23 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  if (video === null) {
+    return res.render("404", { pageTitle: "video not found" });
+  }
   return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 //edit contorollers
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  } //반드시 return을 적어야한다 .
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
 
   return res.redirect(`/videos/${id}`);
 };
