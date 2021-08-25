1caee8fd76423274f0d6036918188e74b8c9e938
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-25 18:37:26 +0900

    session and cookies

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,67 @@
 import User from "../models/User";
-
+import bcrypt from "bcrypt";
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Create Account" });
 };
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
-  const exists = await User.exists({ $or: [{ username }, { email }] });
-  if (exists) {
-    return res.render("join", {
-      pageTitle: "Join",
+  const { name, username, email, password, password2, location } = req.body;
+  const EmailAndUserexists = await User.exists({
+    $or: [{ username }, { email }],
+  });
+  const pageTitle = "join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  if (EmailAndUserexists) {
+    return res.status(400).render("join", {
+      pageTitle,
       errorMessage: "This username or email is already taken",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "join",
+      errorMessage: error._message,
+    });
+  }
 };
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Login" });
+};
+
+export const postLogin = async (req, res) => {
+  const pageTitle = "Login";
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password.",
+    });
+  }
+  return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("edit user");
 export const remove = (req, res) => res.send("Delete user");
-export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.sedn("see profile");
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // express 서버와 연결
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 //router와 연결 시키기
 import rootRouter from "./routers/rootRouters";
 import videoRouter from "./routers/videoRouters";
@@ -15,6 +16,13 @@ app.set("view engine", "pug");
 //middlewares 만들어 주기
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 //router 함수
 app.use("/", rootRouter);
 app.use("/users", userRouter);
