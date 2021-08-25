c5483c4cd3ca7a37530422e9fd75a44202d7bfad
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-25 11:41:01 +0900

    hashing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,21 @@
-export const getJoin = (req, res) =>
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Create Account" });
+};
 export const postJoin = async (req, res) => {
-  console.log(req.body);
-  res.end();
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("edit user");
 export const remove = (req, res) => res.send("Delete user");
-export const login = (req, res) => res.send("Login");
+export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.sedn("see profile");
