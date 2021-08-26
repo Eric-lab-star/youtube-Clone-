2a4ba8aab0fd317b7e4d3befbe3266c7aa1562c0
Eric-lab-star <78295170+Eric-lab-star@users.noreply.github.com>
2021-08-26 16:23:18 +0900

    github login api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
-import { urlencoded } from "express";
+import fetch from "node-fetch";
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Create Account" });
 };
@@ -85,15 +85,29 @@ export const finishGit = async (req, res) => {
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
-  const data = await fetch(finalUrl, {
-    method: "POST",
-    header: {
-      Accept: "application/json",
-    },
-  });
-  const json = await data.json();
-  console.log(json);
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userRequest);
+  } else {
+    return res.redirect("/login");
+  }
 };
+
 export const edit = (req, res) => res.send("edit user");
 export const remove = (req, res) => res.send("Delete user");
 export const logout = (req, res) => res.send("logout");
